ced47188e2d0bc25542cb255fd31dffe5fbc1503
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-08-01 19:29:28 +0800

    refactor: simplify `commands.selectCommand` function

--- src/commands/root.ts
+++ src/commands/root.ts
@@ -2,24 +2,23 @@ import select from "@inquirer/select";
 import * as venom from "venom-bot";
 
 export async function selectCommand() {
-  const command = await select({
+  const trigger = await select({
     message: "Select a command",
     choices: [
-      { name: "Run WhatsApp", value: "run-wa" },
-      { name: "Exit", value: "exit" },
+      {
+        name: "Run WhatsApp",
+        value: async () => {
+          await venom.create({ headless: false, session: "default" });
+        },
+      },
+      {
+        name: "Exit",
+        value: () => {
+          process.exit();
+        },
+      },
     ],
   });
 
-  switch (command) {
-    case "run-wa": {
-      await venom.create({ headless: false, session: "default" });
-      break;
-    }
-    case "exit": {
-      process.exit();
-      break;
-    }
-    default:
-      throw new Error(`Unknown command: ${command}`);
-  }
+  await trigger();
 }
