aae16e6c3d434a5491f7acb5b0bc632beab2461b
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-19 17:25:28 +0700

    test: simplify ESLint configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,28 +1,11 @@
 /* eslint-env node */
 module.exports = {
   root: true,
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:github/recommended",
+  extends: ["eslint:recommended"],
+  overrides: [
+    {
+      files: ["**/*.ts"],
+      extends: ["plugin:@typescript-eslint/recommended"],
+    },
   ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: 9,
-    project: "tsconfig.json",
-    sourceType: "module",
-  },
-  plugins: ["@typescript-eslint"],
-  rules: {
-    camelcase: "off",
-    "i18n-text/no-en": "off",
-    "import/no-namespace": "off",
-    "import/no-unresolved": "off",
-    "no-shadow": "off",
-    "prettier/prettier": "off",
-  },
-  env: {
-    es6: true,
-    node: true,
-  },
 };
